b3b3c199d227335c4a14c7c46853d4aee0f515e2
hariks123 <harikrishna_somayajula@outlook.com>
2021-12-21 18:17:14 -0500

    Adjust Gauze title

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -28,7 +28,8 @@ function Gauze(SampleID) {
         }
     }]
     //Define Layout for Gauze
-    var glayout={ width: 600, height: 450, margin: { t: 0, b: 0 },name:'Scrubs per Week' }
+    var glayout={ //width: 600, height: 450, 
+      margin: { t: 0, b: 0 },name:'Scrubs per Week' }
     Plotly.newPlot("gauge",gdata,glayout)
   
     })
